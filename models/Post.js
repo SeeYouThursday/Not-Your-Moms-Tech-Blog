6ea143d594c721d869265bafec87cc6d255e972c
Brian <galyenb@vcu.edu>
2023-11-25 12:55:23 -0500

    all models have been set up -- todo: routes/FE Logic

--- models/Post.js
+++ models/Post.js
@@ -1,34 +1,50 @@
 //Used for the main blog post
-const { Model, Datatypes } = require("sequelize");
-const sequelize = require("../config/connection");
-const { User, Comments } = require("../models");
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// const { User, Comment } = require("../models");
 
 class Post extends Model {}
 
-Post.init({
-  id: {
-    type: Datatypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user_id: {
-    type: Datatypes.INTEGER,
-    references: {
-      model: User,
-      key: "id",
+Post.init(
+  {
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    comment_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: 'Comment',
+        key: 'id',
+      },
+    },
+    date: {
+      type: Datatypes.DATEONLY,
+      allowNull: false,
+      defaultValue: Datatypes.NOW,
     },
   },
-  post: {
-    type: Datatypes.INTEGER,
-  },
-  comment_id: {
-    type: Datatypes.INTEGER,
-    references: {
-      model: Comments,
-      key: "id",
-    },
-  },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
 
 module.exports = Post;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, Datatypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+    //check to make sure the entered password matches the hashed stored password
+  }
+}
 
 User.init(
   {
@@ -30,9 +35,27 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality -- hash the password
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality -- hash the updated password
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+  },
+  {
     sequelize,
-    timestamps: true,
+    timestamps: false,
     underscored: true,
+    freezeTableName: true,
     modelName: "user",
   }
 );
